4b76c028a590544b5b0146dcf8eda6f5d57349fd
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2023-11-07 16:52:55 +0100

    Update sample project dependency versions

--- 03-suspend-functions/final/app/build.gradle.kts
+++ 03-suspend-functions/final/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.kodeco.android.kotlincoroutinesfundamentals"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
